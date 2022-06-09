3ecab128ff306d2d9e063201e4eae16ba11ec5e5
natruz <88256962+natruz@users.noreply.github.com>
2022-06-09 23:13:45 +0800

    Initial Commit

---
+++ SAP-L8-jokes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SAP-L8-jokes
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 9/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SAP-L8-jokes/SAP_L8_jokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAP_L8_jokesApp.swift
+//  SAP-L8-jokes
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SAP_L8_jokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
